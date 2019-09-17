ca3747c46fbe75322cd7c933fb80288599025db2
sofialevin <slevin@M-4876.local>
2019-09-17 15:00:36 -0700

    Added ability to search for other user.

--- src/App.js
+++ src/App.js
@@ -2,36 +2,20 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import UserCard from './components/UserCard';
+import Followers from './components/Followers';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      follower: "",
+      user: "sofialevin",
       userData: {},
       followers: []
     };
   }
 
   componentDidMount() {
-    fetch("https://api.github.com/users/sofialevin")
-      .then(res => res.json())
-      .then(user => this.setState({
-        userData: user
-      }))
-      .then(
-        fetch("https://api.github.com/users/sofialevin/followers")
-        .then(res => res.json())
-        .then(followers => this.setState({
-          followers: followers
-        }))
-      )
-      .catch(err => console.log("noooo: ", err));
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.follower !== this.state.follower) {
-      fetch(`https://api.github.com/users/${this.state.follower}`)
+    fetch(`https://api.github.com/users/${this.state.user}`)
       .then(res => res.json())
       .then(user => this.setState({
         userData: user
@@ -44,21 +28,39 @@ class App extends React.Component {
         }))
       )
       .catch(err => console.log("noooo: ", err));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.user !== this.state.user) {
+      fetch(`https://api.github.com/users/${this.state.user}`)
+        .then(res => res.json())
+        .then(user => this.setState({
+          userData: user
+        }))
+        .then(
+          fetch(`https://api.github.com/users/${this.state.user}/followers`)
+          .then(res => res.json())
+          .then(followers => this.setState({
+            followers: followers
+          }))
+        )
+        .catch(err => console.log("noooo: ", err));
     }
   }
 
-  updateUser = (user) => {
-    this.setState({
-      user: user
-    })
-  }
+handleClick = (user) => {
+  this.setState({
+    user: user
+  })
+}
+
 
   render() { 
     console.log(this.state.userData)
     return (
       <div>
-        <Route exact path="/" render={() => <UserCard userData={this.state.userData} followers={this.state.followers} updateUser={this.updateUser}/>} />
-        <Route path="/:user" render={() => <UserCard userData={this.state.userData} followers={this.state.followers} />} />
+        <Route exact path="/" render={() => <UserCard userData={this.state.userData} followers={this.state.followers} handleClick={this.handleClick}/>}/>
+        <Route path={`/followers/${this.state.userData.login}`} render={() => <Followers followers={this.state.followers}/>}/>
       </div>
     );
   }
