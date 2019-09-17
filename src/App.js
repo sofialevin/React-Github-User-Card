4ccbfd33feb5e8bcf65d4f90ed7dfd9342a9cd8a
sofialevin <slevin@M-4876.local>
2019-09-17 14:28:44 -0700

    Completed styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import UserCard from './components/UserCard';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: {}
+      follower: "",
+      userData: {},
+      followers: []
     };
   }
 
@@ -16,12 +19,47 @@ class App extends React.Component {
       .then(user => this.setState({
         userData: user
       }))
+      .then(
+        fetch("https://api.github.com/users/sofialevin/followers")
+        .then(res => res.json())
+        .then(followers => this.setState({
+          followers: followers
+        }))
+      )
       .catch(err => console.log("noooo: ", err));
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.follower !== this.state.follower) {
+      fetch(`https://api.github.com/users/${this.state.follower}`)
+      .then(res => res.json())
+      .then(user => this.setState({
+        userData: user
+      }))
+      .then(
+        fetch(`https://api.github.com/users/${this.state.user}/followers`)
+        .then(res => res.json())
+        .then(followers => this.setState({
+          followers: followers
+        }))
+      )
+      .catch(err => console.log("noooo: ", err));
+    }
+  }
+
+  updateUser = (user) => {
+    this.setState({
+      user: user
+    })
+  }
+
   render() { 
+    console.log(this.state.userData)
     return (
-      <div></div>
+      <div>
+        <Route exact path="/" render={() => <UserCard userData={this.state.userData} followers={this.state.followers} updateUser={this.updateUser}/>} />
+        <Route path="/:user" render={() => <UserCard userData={this.state.userData} followers={this.state.followers} />} />
+      </div>
     );
   }
 }
